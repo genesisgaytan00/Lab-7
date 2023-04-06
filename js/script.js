b98fe202aaa679a128e6f6b4cb8f352c268a26cb
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-04-06 11:33:32 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function init(){
     function showMeText(){
       var textbox = document.getElementById('entryinput');
       document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Genesis Gaytan: " + textbox.value);
+      alert("Genesis Gaytan: This is a Test " + textbox.value);
     }
   
     button.addEventListener('click', showMeText);
